a6eba9da101cd156189f3fc4f5a3945c29f8bf1b
HenriJSantos <HenriJSantos>
2019-03-19 15:56:45 +0000

    Exercise 2

--- TP4/example4/MyDiamond.js
+++ TP4/example4/MyDiamond.js
@@ -23,17 +23,17 @@ class MyDiamond extends CGFobject {
 			0, 0, 1,
 		];
 
+		this.texCoords = [
+			0, 0.5,
+			0.25, 0.75,
+			0.25, 0.25,
+			0.5, 0.5
+		];
+
 		//Counter-clockwise reference of vertices
 		this.indices = [
 			0, 1, 3,
 			0, 3, 2
-		];
-
-		this.texCoords = [
-			0, 1,
-			1, 1,
-			0, 0,
-			1, 0
 		];
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
